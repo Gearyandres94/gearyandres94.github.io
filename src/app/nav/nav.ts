3c4f79362aecfb65ce94434be0c271bf5291cf65
Andres Geary <gearyandres@gmail.com>
2026-07-29 00:37:03 -0300

    added nav and footer

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {}
+export class Nav { }
