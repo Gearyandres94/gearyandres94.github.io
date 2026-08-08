473147781a0731dbe5edfdc015ed2013b058eb58
Andres Geary <gearyandres@gmail.com>
2026-08-07 22:07:46 -0300

    started nav styles

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
-  actualLanguage: string = 'En';
-  toggleLanguange() { }
+  lang = new FormControl('english')
 }
